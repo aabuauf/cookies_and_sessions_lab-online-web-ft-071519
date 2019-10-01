5c906cee1341697a588489269f5de25f4e264548
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-01 12:02:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,13 @@ class ApplicationController < ActionController::Base
   helper_method :current_cart
   
   def cart
+  # Get the item from the path
+  @item = Item.find(params[:id])
+ 
+  # Load the cart from the session, or create a new empty cart.
+  current_cart << @item.id
+ 
+  # Save the cart in the session.
+  session[:cart] = cart
   end
 end
